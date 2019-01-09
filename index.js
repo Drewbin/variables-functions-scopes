573ee430d6131b63ccea00574754d9b0bd723719
Samuel Skeen <sskeen@sskeendevelopment.com>
2019-01-08 21:19:33 -0700

    day one result

---
+++ index.js
@@ -0,0 +1,70 @@
+$greeting_4; //available; value: undefined
+
+
+var $greeting_4 = "Hey howdy";
+
+
+
+
+
+const daysOfWeek = 7;
+
+// const event = {
+    // start: new Date(),
+    // end: new Date() + 8 days,
+// };
+
+// if (event.end - event.start > daysOfWeek) {
+
+// }
+
+function multiply(num1,num2){
+    const localVar2 = { key: 'constant value' };
+
+    localVar2 = 'blah'; // Error!
+    
+    if (num1 > num2) {
+    }
+
+    localVar;//error!
+
+    let localVar;
+    
+    i;//error!
+    
+    for (const i = 0; i < Array.length; i++) {
+
+    }
+
+    for (const key in obj) {
+
+    }
+
+    i;// error!
+
+    // var arbitraryVar;
+    
+    return num1 * num2;
+
+    function innerFunc() {
+        var inner;
+    }
+}
+multiply(3,6);
+
+42 == 42;
+
+greeting = false;
+greeting(); //error!
+var greeting = () => {};
+greeting = function someName() {
+    // someName()
+};
+someName();//error!
+greeting = [];
+
+greeting = {};
+
+greeting = undefined;
+
+greeting = null;
